493a5d6e1252b7ee9b30080937daf60696886892
root <root@phantom.localdomain>
2020-09-02 12:45:43 +0000

    New action and widgets

--- sophoscentral_connector.py
+++ sophoscentral_connector.py
@@ -568,6 +568,39 @@ class SophosCentralConnector(BaseConnector):
         # BaseConnector will create a textual message based off of the summary dictionary
         return action_result.set_status(phantom.APP_SUCCESS)
 
+    def _handle_get_web_categories(self, param):
+        # Implement the handler here
+        # use self.save_progress(...) to send progress messages back to the platform
+        self.save_progress("In action handler for: {0}".format(self.get_action_identifier()))
+
+        # Add an action result object to self (BaseConnector) to represent the action for this param
+        action_result = self.add_action_result(ActionResult(dict(param)))
+
+        # Get API token
+        token = self._get_token(param, action_result)
+
+        query = "/endpoint/v1/settings/web-control/categories"
+        headers = {"X-Tenant-ID": self._tenantID, "Authorization": "Bearer " + token}
+        # make rest call
+        ret_val, response = self._make_rest_call_new(
+            query, action_result, method="get", headers=headers
+        )
+
+        if phantom.is_fail(ret_val):
+            # the call to the 3rd party device or service failed, action result should contain all the error details
+            return action_result.get_status()
+
+        # Add the response into the data section
+        action_result.add_data(response)
+
+        # Add a dictionary that is made up of the most important values from data into the summary
+        summary = action_result.update_summary({})
+        summary['num_data'] = ret_val
+
+        # Return success, no need to set the message, only the status
+        # BaseConnector will create a textual message based off of the summary dictionary
+        return action_result.set_status(phantom.APP_SUCCESS)
+
     def _handle_tamper_protection_globally(self, param):
         # Implement the handler here
         # use self.save_progress(...) to send progress messages back to the platform
@@ -1031,6 +1064,9 @@ class SophosCentralConnector(BaseConnector):
 
         elif action_id == 'tamper_protection_globally':
             ret_val = self._handle_tamper_protection_globally(param)
+
+        elif action_id == 'get_web_categories':
+            ret_val = self._handle_get_web_categories(param)
 
         return ret_val
 
